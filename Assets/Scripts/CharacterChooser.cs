b8879efe2bdcaefc9dae618be9834ffa63dfd0a9
Aasharib <aasharib@icloud.com>
2024-09-14 19:33:49 +0500

    feat: implemented character choose with command line argument

--- Assets/Scripts/CharacterChooser.cs
+++ Assets/Scripts/CharacterChooser.cs
@@ -8,13 +8,21 @@ public class CharacterChooser : MonoBehaviour
 
     [SerializeField] private FrameReader frameReader;
     [SerializeField] private SlideShow slideShow;
+    [SerializeField] public int commandLineCharacter = -1;
+
     private void Start()
     {
         slideShow.onSelection += OnCharacterSelect;
+        slideShow.SetCharacterIndex(commandLineCharacter);
     }
 
     private void OnCharacterSelect(int index,GameObject node)
     {
         frameReader.SetNewCharacter(Instantiate(node));
+        UIManager.Instancce.OnSlideShowExit(this.gameObject);
+    }
+
+    public void setCommandLineCharacterIndex(int cmdIndex) { 
+        this.commandLineCharacter = cmdIndex;
     }
 }
--- Assets/Scripts/SlideShow.cs
+++ Assets/Scripts/SlideShow.cs
@@ -150,6 +150,11 @@ public class SlideShow : MonoBehaviour
             nodes.Add(newNodes[i]);
         }
     }
+
+    public void SetCharacterIndex(int index) { 
+        this.index = index;
+        OnSelectItem();
+    }
 }
 
 #if UNITY_EDITOR
